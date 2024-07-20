a8da9bb6cab685f879aa81301da6a64ed8d5f0ef
nikdudek <nik.dudek@gmail.com>
2024-07-20 21:29:53 +0200

    Initial commit. Added .gitignore and removed IDE-specific files

---
+++ src/practice_project/HelloWorld.java
@@ -0,0 +1,11 @@
+package practice_project;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!\n");
+
+	}
+
+}
